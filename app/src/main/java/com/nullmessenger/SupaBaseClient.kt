c56e7fcf0d8c68c857b336d6ff1d6ffb70fd8caf
NullSec11 <cretu.maxim09@gmail.com>
2026-07-26 18:56:15 +0300

    Refactor SupabaseClient initialization

--- app/src/main/java/com/nullmessenger/SupaBaseClient.kt
+++ app/src/main/java/com/nullmessenger/SupaBaseClient.kt
@@ -1,12 +1,15 @@
 package com.nullmessenger
 
+
 import io.github.jan.supabase.SupabaseClient
 import io.github.jan.supabase.createSupabaseClient
 import io.github.jan.supabase.auth.Auth
 
+
 val supabase: SupabaseClient = createSupabaseClient(
-    supabaseUrl = "https://ktdwlztlehvpnjwjwbzj.supabase.co",
-    supabaseKey = "sb_publishable_UQtIjulNbc86LK_3eTpRlg_ld3WuoDM"
+supabaseUrl = "https://ktdwlztlehvpnjwjwbzj.supabase.co",
+supabaseKey = "sb_publishable_UQtIjulNbc86LK_3eTpRlg_ld3WuoDM"
 ) {
-    install(Auth)
+install(Auth)
 }
+
