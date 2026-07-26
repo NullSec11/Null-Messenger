b6e6106caf148cf96227a49e5e4afd64287f6f76
NullSec11 <cretu.maxim09@gmail.com>
2026-07-26 18:36:13 +0300

    Initialize Supabase client in SupaBaseClient.kt

--- app/src/main/java/com/nullmessenger/SupaBaseClient.kt
+++ app/src/main/java/com/nullmessenger/SupaBaseClient.kt
@@ -1,1 +1,9 @@
 package com.nullmessenger
+
+import io.github.jan.supabase.SupabaseClient
+import io.github.jan.supabase.createSupabaseClient
+
+val supabase: SupabaseClient = createSupabaseClient(
+    supabaseUrl = "https://ktdwlztlehvpnjwjwbzj.supabase.co",
+    supabaseKey = "sb_publishable_UQtIjulNbc86LK_3eTpRlg_ld3WuoDM"
+)
