c1db3cc356d80e0689bff128a19914f2048af0c4
NullSec11 <cretu.maxim09@gmail.com>
2026-07-26 18:47:21 +0300

    Install Auth feature in SupabaseClient

--- app/src/main/java/com/nullmessenger/SupaBaseClient.kt
+++ app/src/main/java/com/nullmessenger/SupaBaseClient.kt
@@ -2,8 +2,11 @@ package com.nullmessenger
 
 import io.github.jan.supabase.SupabaseClient
 import io.github.jan.supabase.createSupabaseClient
+import io.github.jan.supabase.auth.Auth
 
 val supabase: SupabaseClient = createSupabaseClient(
     supabaseUrl = "https://ktdwlztlehvpnjwjwbzj.supabase.co",
     supabaseKey = "sb_publishable_UQtIjulNbc86LK_3eTpRlg_ld3WuoDM"
-)
+) {
+    install(Auth)
+}
