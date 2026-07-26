4ce8126ef1a3a9f14bb9cd556ad296e7b2253fcb
NullSec11 <cretu.maxim09@gmail.com>
2026-07-26 17:47:50 +0300

    Remove Supabase client initialization code

--- app/src/main/java/com/nullmessenger/SupaBaseClient.kt
+++ app/src/main/java/com/nullmessenger/SupaBaseClient.kt
@@ -1,17 +1,1 @@
 package com.nullmessenger
-
-import io.github.jan.supabase.auth.Auth
-import io.github.jan.supabase.createSupabaseClient
-import io.github.jan.supabase.postgrest.Postgrest
-import io.github.jan.supabase.realtime.Realtime
-import io.github.jan.supabase.storage.Storage
-
-val supabase = createSupabaseClient(
-    supabaseUrl = "https://ktdwlztlehvpnjwjwbzj.supabase.co",
-    supabaseKey = "sb_publishable_UQtIjulNbc86LK_3eTpRlg_ld3WuoDM"
-) {
-    install(Auth)
-    install(Postgrest)
-    install(Realtime)
-    install(Storage)
-}
