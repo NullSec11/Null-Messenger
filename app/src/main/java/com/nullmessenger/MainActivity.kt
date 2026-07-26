88b90ad8850635da008c1c68cd2d2ccfbe6c6e78
NullSec11 <cretu.maxim09@gmail.com>
2026-07-26 17:01:31 +0300

    Replace NullMessengerTheme with MaterialTheme

--- app/src/main/java/com/nullmessenger/MainActivity.kt
+++ app/src/main/java/com/nullmessenger/MainActivity.kt
@@ -3,17 +3,17 @@ package com.nullmessenger
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 
 class MainActivity : ComponentActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContent {
-            NullMessengerTheme {
+            MaterialTheme {
                 Surface {
-                    NullMessengerApp()
+                    AppUi()
                 }
             }
         }
